70f2032023ec49763408ae59c074d3f612e210be
Tim Aerts <aardbeiplantje@gmail.com>
2025-11-06 20:26:35 +0100

    Dockerfile/nginx.conf: use letsencrypt style, add that in deploy.sh too

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM alpine:latest AS proxy-runtime
 RUN apk add --no-cache nginx nginx-mod-http-auth-jwt nginx-mod-http-headers-more nginx-mod-http-lua
 COPY ./nginx.conf /etc/nginx/nginx.conf
-RUN \
-    mkdir -p /etc/nginx/certs; \
-    nginx -t -c /etc/nginx/nginx.conf
+RUN ln -s /certs/ /etc/nginx/certs
+RUN nginx -t -c /etc/nginx/nginx.conf
 ENTRYPOINT ["/usr/sbin/nginx", "-c", "/etc/nginx/nginx.conf", "-g", "daemon off;"]
--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,9 @@ export STACK_NAME=${STACK_NAME:-$APP_NAME}
 printf -v now "%(%s)T" -1
 export CFG_PREFIX=$STACK_NAME-$now
 export STACK_CONFIG=${STACK_CONFIG:-$WORKSPACE}
-export STACK_CERTS=${STACK_CERTS:-$WORKSPACE}
-export REGISTRY_HTTP_SECRET=${REGISTRY_HTTP_SECRET:-$(uuidgen)}
-docker secret create \
-    --template-driver golang \
-    registry-https-crt-$CFG_PREFIX \
-    ${HTTPS_CRT:-$STACK_CERTS/$STACK_NAME-crt.pem}
-docker secret create \
-    --template-driver golang \
-    registry-https-key-$CFG_PREFIX \
-    ${HTTPS_KEY:-$STACK_CERTS/$STACK_NAME-key.pem}
+ipv6_prefix=${APP_IPV6_PREFIX?Need APP_IPV6_PREFIX}
+
+echo "creating configs and secrets with prefix $CFG_PREFIX"
 docker config create \
     --template-driver golang \
     registry-proxy-config-$CFG_PREFIX \
@@ -26,29 +19,94 @@ docker secret create \
     --template-driver golang \
     registry-auth-htpasswd-$CFG_PREFIX \
     <(echo "$REGISTRY_USER:$HASHED_PASS")
-if [ ! -z "$DOCKER_REGISTRY" -a ! -z "$DOCKER_REGISTRY_USER" -a ! -z "$DOCKER_REGISTRY_PASS" ]; then
-    export DOCKER_REGISTRY_PASS DOCKER_REGISTRY_USER DOCKER_REGISTRY
-    printenv DOCKER_REGISTRY_PASS \
-        |docker login -u $DOCKER_REGISTRY_USER $DOCKER_REGISTRY --password-stdin
-else
-    export DOCKER_REGISTRY=${DOCKER_REGISTRY:-local}
-    export DOCKER_REPO=${DOCKER_REPO:-registry}
-fi
-export DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-registry}
-export DOCKER_REGISTRY
 
-echo "clean up $STACK_NAME"
+echo "checking for registry auth"
+if [   -z "$DOCKER_REGISTRY" \
+    -o -z "$DOCKER_REGISTRY_USER" \
+    -o -z "$DOCKER_REGISTRY_PASS" ]; then
+    echo "no registry login info, using local registry"
+    export DOCKER_REGISTRY=local
+else
+    echo "using registry login info"
+    export DOCKER_REGISTRY
+    export DOCKER_REGISTRY_USER
+    export DOCKER_REGISTRY_PASS
+    printenv DOCKER_REGISTRY_PASS \
+        |docker login \
+            -u ${DOCKER_REGISTRY_USER?Need a DOCKER_REGISTRY_USER} \
+            --password-stdin \
+            ${DOCKER_REGISTRY?Need a DOCKER_REGISTRY}
+fi
+
+echo "removing old stack, as we need to recreate the network with same IPv6"
 docker stack rm $STACK_NAME 2>/dev/null
 
+nw_name=dmz-ipv6-$STACK_NAME
 if_name=${BRIDGE_IF_NAME:-dmz-${STACK_NAME}0}
-nw_name=dmz-$STACK_NAME
-ipv6_prefix=${IPV6_PREFIX:-fd53:7cb8:383:eb00:abcd::/120}
-echo "network name: $nw_name if_name: $if_name ipv6_prefix: $ipv6_prefix"
+echo "removing network name: $nw_name if_name: $if_name ipv6_prefix: $ipv6_prefix"
 docker network rm $nw_name 2>/dev/null
 docker network ls --filter name=$nw_name -q
 while [ $(docker network ls --filter name=$nw_name -q|wc -l) -gt 0 ]; do
     sleep 1
 done
+echo "removed network $nw_name"
+
+if [ "${APP_DO_CERTBOT:-0}" -eq 1 ]; then
+    APP_CERTBOT_MAIL=${APP_CERTBOT_MAIL?Need APP_CERTBOT_MAIL}
+    DOMAIN=${APP_DOMAIN?Need APP_DOMAIN}
+    IPV6=$(dig -6 $DOMAIN -t AAAA +short +retry=0 +tries=1)
+    if [ $? != 0 -o -z "$IPV6" ]; then
+        echo -ne "problem looking up $DOMAIN:\n$IPV6\n"
+        exit 1
+    fi
+    APP_IF_NAME=${APP_IF_NAME:-eno1}
+    echo "checking for network, using $APP_IF_NAME"
+    echo "running sysctl for proxy_ndp and add $IPV6 as neighbour"
+    sudo sh -x -c "ip -6 neigh add proxy $IPV6 dev $APP_IF_NAME; sysctl net.ipv6.conf.default.proxy_ndp=1; sysctl net.ipv6.conf.all.proxy_ndp=1;"
+    # work via export ENV, as this is a possible secret, and we dont want to
+    # show this in a ps or log
+    echo "removing network $nw_name"
+    docker network rm $nw_name
+    echo "creating network $nw_name with prefix $ipv6_prefix"
+    docker network create \
+        --ipv6 $nw_name \
+        --subnet=$ipv6_prefix \
+        --ipv4=false \
+        --attachable=true \
+        --scope=local \
+        --driver=bridge \
+        -o com.docker.network.bridge.name=$APP_NAME-dmz0 \
+        -o com.docker.network.container_iface_prefix=dmz \
+        -o com.docker.network.bridge.gateway_mode_ipv6=routed \
+        -o com.docker.network.bridge.enable_icc=true \
+        -o com.docker.network.bridge.enable_ip_masquerade=false \
+        -o com.docker.network.bridge.enable_ip6_masquerade=false \
+        -o com.docker.network.enable_ipv6=true \
+        -o com.docker.network.bridge.inhibit_ipv4=true \
+        -o com.docker.network.driver.mtu=1500 \
+        --ipam-driver default || exit $?
+    echo "check the certtificate"
+    docker run \
+        -p 80:80 \
+        --pull=always \
+        --rm \
+        -e CERTBOT_MAIL="$APP_CERTBOT_MAIL" \
+        --network $nw_name \
+        --ip6 $IPV6 \
+        --name certbot_$APP_NAME \
+        -v ${APP_CERT_VOLUME}:/certs:rw \
+        ghcr.io/aardbeiplantje/certbot/certbot:${APP_CERTBOT_TAG:-latest} \
+            certonly \
+                --agree-tos \
+                --force-renewal \
+                --domains "$DOMAIN" || exit $?
+    echo "removing network $nw_name"
+    docker network rm $nw_name
+else
+    echo "no certbot"
+fi
+
+echo "creating network $nw_name with prefix $ipv6_prefix"
 docker network create \
     $nw_name \
     --ipv6 \
@@ -67,6 +125,10 @@ docker network create \
     -o com.docker.network.bridge.inhibit_ipv4=false \
     -o com.docker.network.driver.mtu=1500 \
     --ipam-driver default
+
+echo "make sure the container image comes from local"
+export DOCKER_REGISTRY=local
+export DOCKER_REPOSITORY=registry
 
 echo "stack deploy $STACK_NAME"
 docker stack deploy \
